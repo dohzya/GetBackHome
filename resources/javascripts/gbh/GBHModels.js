87d5ea4d967e973a7d4857bd17e13cc92fc9ea40
Étienne Vallette d'Osia <evo@zenexity.com>
2013-06-11 21:06:08 +0200

    Begin the add of XP

--- resources/javascripts/gbh/GBHModels.js
+++ resources/javascripts/gbh/GBHModels.js
@@ -90,12 +90,25 @@ app.service("GBHModels", ["$rootScope", "GBHLogger", function ($rootScope, Logge
     this.fighting = args.fighting;
     this.tooling = args.tooling;
     this.level = args.level;
+    this.xp = args.xp;
+    this.health = args.health;
+    this.currentHealth = args.currentHealth;
+    this.mental = args.mental;
+    this.currentMental = args.currentMental;
   }
   Survivor.prototype.Defense = function() {
     return this.fighting.defense;
   };
   Survivor.prototype.Attack = function() {
     return this.fighting.attack;
+  };
+  Survivor.prototype.AddXp = function(xp) {
+    this.xp += xp;
+    if (this.xp > Math.pow(2, this.level)) {
+      this.level++;
+      this.health += 0.2;
+      this.mental += 0.2;
+    }
   };
   function createSurvivor(args) {
     return new Survivor(args);
@@ -107,7 +120,12 @@ app.service("GBHModels", ["$rootScope", "GBHLogger", function ($rootScope, Logge
         food: 30,
         fighting: createFighting("survivor"),
         tooling: 0.5,
-        level: 1
+        level: 1,
+        xp: 0,
+        health: 1,
+        currentHealth: 0.5,
+        mental: 1,
+        currentMental: 0.5
       }));
     }
     return survivors;
