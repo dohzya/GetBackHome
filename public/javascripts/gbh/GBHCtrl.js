97055589e893b0b5dc37d82ca845e692e684a896
Étienne Vallette d'Osia <evo@zenexity.com>
2013-08-08 08:57:04 +0200

    One controller to bind them all.

--- public/javascripts/gbh/GBHCtrl.js
+++ public/javascripts/gbh/GBHCtrl.js
@@ -1,36 +1,16 @@
-app.controller("GBHCtrl", ["$scope", "GBHEngine", function ($scope, Engine) {
+app.controller("GBHCtrl", ["$scope", function ($scope) {
   "use strict";
 
-  var inAction = false;
-
-  function doAction(func) {
-    return function() {
-      if (!inAction) {
-        inAction = true;
-        func.apply(this, arguments);
-        inAction = false;
-      }
-    };
-  }
-
-  $scope.game = {
-    selectedSurvivors: Engine.selectSurvivors(0),
-    selectedOrder: null
+  $scope.views = {
+    selectedView: "orders"
   };
 
-  $scope.nextTurn = doAction(function(){ Engine.turn(); });
-
-  $scope.sendOrder = doAction(function(id){
-    Engine.sendOrder(id);
-    $scope.game.selectedSurvivors = Engine.selectSurvivors(0);
-  });
-
-  $scope.selectSurvivors = doAction(function(selectedSurvivors){
-    $scope.game.selectedSurvivors = Engine.selectSurvivors(selectedSurvivors);
-  });
-
-  $scope.selectOrder = doAction(function(selectedOrder){
-    $scope.currentOrder = Engine.selectOrder(selectedOrder);
-  });
+  $scope.selectView = function(name) {
+    $scope.views.selectedView = name;
+  };
+  $scope.viewClass = function(name) {
+    if (name === $scope.views.selectedView) { return "show"; }
+    else { return "hide"; }
+  };
 
 }]);
---
+++ public/javascripts/gbh/GBHOrdersCtrl.js
@@ -0,0 +1,36 @@
+app.controller("GBHOrders", ["$scope", "GBHEngine", function ($scope, Engine) {
+  "use strict";
+
+  var inAction = false;
+
+  function doAction(func) {
+    return function() {
+      if (!inAction) {
+        inAction = true;
+        func.apply(this, arguments);
+        inAction = false;
+      }
+    };
+  }
+
+  $scope.game = {
+    selectedSurvivors: Engine.selectSurvivors(0),
+    selectedOrder: null
+  };
+
+  $scope.nextTurn = doAction(function(){ Engine.turn(); });
+
+  $scope.sendOrder = doAction(function(id){
+    Engine.sendOrder(id);
+    $scope.game.selectedSurvivors = Engine.selectSurvivors(0);
+  });
+
+  $scope.selectSurvivors = doAction(function(selectedSurvivors){
+    $scope.game.selectedSurvivors = Engine.selectSurvivors(selectedSurvivors);
+  });
+
+  $scope.selectOrder = doAction(function(selectedOrder){
+    $scope.currentOrder = Engine.selectOrder(selectedOrder);
+  });
+
+}]);
