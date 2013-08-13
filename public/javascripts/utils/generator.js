e33b7e49a84598a20eef5be190d63ac3f6d3a7f0
Étienne Vallette d'Osia <evo@zenexity.com>
2013-08-13 09:22:41 +0200

    Improve generation

--- public/javascripts/utils/generator.js
+++ public/javascripts/utils/generator.js
@@ -5,13 +5,13 @@ app.service("UTGenerator", ["UTPerlinSimplex", "Rc4Random", "GBHModels", functio
     PerlinSimplex.noiseDetail();
     var seedRand = Rc4Random.create(seed);
     var rand = {
-      random: function(){ return seedRand.getRandomNumber(); }
+      random: function () { return seedRand.getRandomNumber(); }
     };
     PerlinSimplex.setRng(rand);
   }
 
   function gen(kind) {
-    return function(x, y) {
+    return function (x, y) {
       return PerlinSimplex.noise(kind, x, y);
     };
   }
@@ -22,69 +22,66 @@ app.service("UTGenerator", ["UTPerlinSimplex", "Rc4Random", "GBHModels", functio
     var type1Gen = gen(1);
     var type2Gen = gen(2);
     var youthGen = gen(3);
+    var attackGen = gen(4);
+    var defenseGen = gen(5);
+    var foodGen = gen(6);
     var zones = [];
-    for (var xx=minX; xx<maxX; xx++) {
-      for (var yy=minY; yy<maxY; yy++) {
-        var x = xx - 10;
-        var y = yy - 10;
-        var infection = parseInt(Math.abs(infectionGen(x, y) * 100), 10);
-        var youth = parseInt(Math.abs(youthGen(x, y) * 100), 10) * 10;
-        var height = parseInt((type1Gen(x, y) * 130 + 200), 10) * 2;
-        var types;
-        var type2Int = parseInt((type2Gen(x, y) * 1000), 10);
-        var t2;
+    var xx, yy;
+    var x, y, infection, youth, height, types, type2Int, attack, defense, food, t2, zone;
+    for (xx = minX; xx < maxX; xx++) {
+      for (yy = minY; yy < maxY; yy++) {
+        x = xx - 10;
+        y = yy - 10;
+        infection = parseInt(Math.abs(infectionGen(x, y) * 100), 10);
+        youth = parseInt(Math.abs(youthGen(x, y) * 100), 10) * 10;
+        height = parseInt((type1Gen(x, y) * 1000), 10) - 50;
+        type2Int = parseInt((type2Gen(x, y) * 1000), 10);
+        attack = attackGen(x, y);
+        attack = attack * attack;  // lower the value (but keeping 1 possible)
+        defense = defenseGen(x, y);
+        defense = defense * defense;  // lower the value (but keeping 1 possible)
+        food = parseInt((foodGen(x, y) * 100), 10);
         if (height < 24) {
-          if (type2Int > 300) {
+          if (type2Int > 950) {
             t2 = "city";
-          }
-          else {
+          } else {
             t2 = null;
           }
           types = ["water", t2];
-        }
-        else if (height < 30) {
+        } else if (height < 30) {
           t2 = null;
           types = ["swamp", t2];
-        }
-        else if (height < 600) {
-          if (type2Int < -200) {
+        } else if (height < 600) {
+          if (type2Int < 100) {
             t2 = "city";
-          }
-          else if (type2Int < -100) {
+          } else if (type2Int < 300) {
             t2 = "forrest";
-          }
-          else if (type2Int < -50) {
+          } else if (type2Int < 500) {
             t2 = "field";
-          }
-          else {
+          } else {
             t2 = null;
           }
           types = ["plain", t2];
-        }
-        else {
-          if (type2Int < -400) {
+        } else {
+          if (type2Int < 100) {
             t2 = "city";
-          }
-          else if (type2Int < -200) {
+          } else if (type2Int < 600) {
             t2 = "forrest";
-          }
-          else if (height > 750) {
+          } else if (height > 800) {
             t2 = "mountains";
-          }
-          else {
+          } else {
             t2 = null;
           }
           types = ["mountainous", t2];
         }
-        var zone = {
+        zone = {
           fighting: {
-            attack: 0,  // TODO generate
-            defense: 0.7  // TODO generate
+            attack: attack,
+            defense: defense
           },
-          food: 100,  // TODO generate
+          food: food,
           height: height,
           horde: Models.createHorde(infection),  // TODO generate
-          infection: infection,
           pos: [x, y],
           ts: 0,
           types: types,
